441a34b2e5fb41481478a7a9831fe0775a47aeaa
MohammedThalha2002 <mohammedalithalha2002@gmail.com>
2023-02-08 14:42:17 +0530

    commit

--- src/FormPage.jsx
+++ src/FormPage.jsx
@@ -25,46 +25,48 @@ function FormPage({ setInputsOpen }) {
       Disease: disease,
       Treatment: treatment,
     };
-    console.log("form value submitted" ,formvalue);
-    axios.post("http://localhost:3000/postdata", formvalue).then((res) => {
-      console.log(res);
-      setInputsOpen(false);
-    }).catch((err) => {
-      setInputsOpen(false);
-      alert("Error : Something went wrong")
-    });
+    console.log("form value submitted", formvalue);
+    axios
+      .post("http://localhost:3000/postdata", formvalue)
+      .then((res) => {
+        console.log(res);
+        setInputsOpen(false);
+      })
+      .catch((err) => {
+        setInputsOpen(false);
+        alert("Error : Something went wrong");
+      });
   }
   return (
     <div>
-      <div className=" z-50 p-[10px] absolute top-[25vh] left-[370px] h-[400px] w-[750px] bg-[#E2E5DE] rounded-xl">
-        <div className="flex items-center justify-center mx-auto w-full max-w-[550px] p-[20px]">
+      <div className=" z-50 p-[20px] absolute top-1/2 left-1/2 translate-y-1/2 -translate-x-1/2 h-[450px] w-[750px] bg-[#E2E5DE] rounded-xl">
+        <div className="flex items-center w-full  p-[20px]">
           <form onSubmit={handleSubmit}>
-            <div className="-mx-3 flex flex-wrap">
-              <div className="w-[190px] px-[10px] sm:w-[100px]">
-                <div className="mb-5">
-                  <label
-                    htmlFor="ID"
-                    className="mb-3 block text-base font-medium text-[#07074D]"
-                  >
-                    ID :
-                  </label>
-                  <input
-                    type="text"
-                    name="fName"
-                    id="fName"
-                    onChange={(val) => {
-                      setId(val.target.value);
-                    }}
-                    placeholder="ID"
-                    className="w-[190px] rounded-md border border-[#e0e0e0] bg-white py-[10px] px-[10px] text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md"
-                  />
-                </div>
+            <div className="flex w-[700px] justify-between">
+              <div className="mb-5">
+                <label
+                  htmlFor="ID"
+                  className="mb-3 block text-base font-medium text-[#07074D] w-[190px]"
+                >
+                  ID :
+                </label>
+                <input
+                  type="text"
+                  name="fName"
+                  id="fName"
+                  onChange={(val) => {
+                    setId(val.target.value);
+                  }}
+                  placeholder="ID"
+                  className="rounded-md border border-[#e0e0e0] w-[190px]
+                    bg-white py-[10px] px-[10px] text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md"
+                />
               </div>
-              <div className="w-[190px] px-3 sm:w-[100px] ml-[110px]">
+              <div className="w-[190px] ">
                 <div className="mb-5">
                   <label
                     htmlFor="lName"
-                    className="mb-3 block text-base font-medium text-[#07074D] w-[200px]"
+                    className="mb-3 block text-base font-medium text-[#07074D] w-[190px] "
                   >
                     Patient Name :
                   </label>
@@ -76,15 +78,17 @@ function FormPage({ setInputsOpen }) {
                     onChange={(val) => {
                       setName(val.target.value);
                     }}
-                    className="w-[190px] rounded-md border border-[#e0e0e0] bg-white py-[10px] px-[10px] text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md"
+                    className="rounded-md border border-[#e0e0e0] bg-white py-[10px] px-[10px]
+                    text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] 
+                    focus:shadow-md w-[190px]"
                   />
                 </div>
               </div>
-              <div className="w-[126px] px-[10px] sm:w-[100px] ml-[115px]">
+              <div className="w-[150px]">
                 <div className="mb-5">
                   <label
                     htmlFor="guest"
-                    className="mb-3 block text-base font-medium text-[#07074D] w-[126px]"
+                    className="mb-3 block text-base font-medium text-[#07074D] w-[150px]"
                   >
                     Temperature :
                   </label>
@@ -96,34 +100,34 @@ function FormPage({ setInputsOpen }) {
                       setTemperature(val.target.value);
                     }}
                     placeholder="92°C"
-                    className="w-[126px] appearance-none rounded-md border border-[#e0e0e0] bg-white py-[10px] px-[10px] text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md"
+                    className="rounded-md border border-[#e0e0e0] w-[120px]
+                     bg-white py-[10px] px-[10px] text-base font-medium text-[#6B7280] 
+                     outline-none focus:border-[#6A64F1] focus:shadow-md"
                   />
                 </div>
               </div>
             </div>
-            <div className="-mx-3 flex flex-wrap">
-              <div className="w-[100px] px-[10px] sm:w-[100px]">
-                <div className="mb-5">
-                  <label
-                    htmlFor="guest"
-                    className="mb-3 block text-base font-medium text-[#07074D]"
-                  >
-                    Age :
-                  </label>
-                  <input
-                    type="number"
-                    name="guest"
-                    id="guest"
-                    placeholder="5"
-                    onChange={(val) => {
-                      setAge(val.target.value);
-                    }}
-                    min="0"
-                    className="w-[100px] appearance-none rounded-md border border-[#e0e0e0] bg-white py-[10px] px-[10px] text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md"
-                  />
-                </div>
+            <div className="flex justify-between w-[671px]">
+              <div className="mb-5">
+                <label
+                  htmlFor="guest"
+                  className="mb-3 block text-base font-medium text-[#07074D] w-[100px]"
+                >
+                  Age :
+                </label>
+                <input
+                  type="number"
+                  name="guest"
+                  id="guest"
+                  placeholder="5"
+                  onChange={(val) => {
+                    setAge(val.target.value);
+                  }}
+                  min="0"
+                  className="w-[100px] rounded-md border border-[#e0e0e0] bg-white py-[10px] px-[10px] text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md"
+                />
               </div>
-              <div className="w-[200px] px-3 sm:w-[160px] ml-[30px]">
+              <div className="w-[200px]">
                 <div className="mb-5">
                   <label
                     htmlFor="lName"
@@ -138,20 +142,21 @@ function FormPage({ setInputsOpen }) {
                       setGender(val.target.value);
                       console.log(val.target.value);
                     }}
-                    className="rounded-md border border-[#e0e0e0] bg-white py-[10px] px-[30px] text-left text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md"
+                    className="rounded-md border border-[#e0e0e0] bg-white py-[10px] px-[30px] text-left
+                     text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md w-[200px]"
                   >
                     <option value="">Gender</option>
-                    <option value="Female">Female</option>
+                    <option value="Femlae">Female</option>
                     <option value="Male">Male</option>
                     <option value="Others">Others</option>
                   </select>
                 </div>
               </div>
-              <div className="w-[100px] px-3 sm:w-[100px] ml-[15px]">
+              <div className="w-[200px]">
                 <div className="mb-5">
                   <label
                     htmlFor="date"
-                    className="mb-3 block text-base font-medium text-[#07074D]"
+                    className="mb-3 block text-base font-medium text-[#07074D] w-[200px]"
                   >
                     Date
                   </label>
@@ -162,33 +167,33 @@ function FormPage({ setInputsOpen }) {
                     onChange={(val) => {
                       setDate(val.target.value);
                     }}
-                    className="rounded-md border border-[#e0e0e0] bg-white py-[9px] px-[50px] text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md"
+                    className="rounded-md border border-[#e0e0e0] bg-white py-[10px] px-[10px] text-base 
+                    font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md w-[200px]"
                   />
                 </div>
               </div>
             </div>
-            <div className="-mx-3 flex flex-wrap">
-              <div className="w-[120px] px-[10px] sm:w-[100px]">
-                <div className="mb-5">
-                  <label
-                    htmlFor="ID"
-                    className="mb-3 block text-base font-medium text-[#07074D] w-[200px]"
-                  >
-                    Blood Pressure :
-                  </label>
-                  <input
-                    type="text"
-                    name="fName"
-                    id="fName"
-                    onChange={(val) => {
-                      setBloodPressure(val.target.value);
-                    }}
-                    placeholder="BP"
-                    className="w-[120px] rounded-md border border-[#e0e0e0] bg-white py-[10px] px-[10px] text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md"
-                  />
-                </div>
+            <div className="flex justify-between w-[671px]">
+              <div className="mb-5 w-[170px]">
+                <label
+                  htmlFor="ID"
+                  className="mb-3 block text-base font-medium text-[#07074D] w-[170px]"
+                >
+                  Blood Pressure :
+                </label>
+                <input
+                  type="text"
+                  name="fName"
+                  id="fName"
+                  onChange={(val) => {
+                    setBloodPressure(val.target.value);
+                  }}
+                  placeholder="BP"
+                  className="w-[120px] rounded-md border border-[#e0e0e0] bg-white py-[10px] px-[10px] text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md"
+                />
               </div>
-              <div className="w-[180px] px-3 sm:w-[100px] ml-[43px]">
+
+              <div className="w-[180px]">
                 <div className="mb-5">
                   <label
                     htmlFor="lName"
@@ -208,11 +213,11 @@ function FormPage({ setInputsOpen }) {
                   />
                 </div>
               </div>
-              <div className="w-[200px] px-3 sm:w-[100px] ml-[103px]">
+              <div className="w-[200px]">
                 <div className="mb-5">
                   <label
                     htmlFor="lName"
-                    className="mb-3 block text-base font-medium text-[#07074D] w-[200px]"
+                    className="mb-3 block text-base font-medium text-[#07074D] w-[170px]"
                   >
                     Treatment :
                   </label>
@@ -224,15 +229,19 @@ function FormPage({ setInputsOpen }) {
                     onChange={(val) => {
                       setTreatment(val.target.value);
                     }}
-                    className="w-[200px] rounded-md border border-[#e0e0e0] bg-white py-[10px] px-[10px] text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md"
+                    className="w-[200px] rounded-md border border-[#e0e0e0] 
+                    bg-white py-[10px] px-[10px] text-base font-medium text-[#6B7280]
+                     outline-none focus:border-[#6A64F1] focus:shadow-md"
                   />
                 </div>
               </div>
             </div>
             <div>
               <input
-                className="hover:shadow-form rounded-md bg-[#07074D] py-[10px] px-8 text-center text-base font-semibold text-white outline-none ml-[430px]"
+                className="hover:shadow-form rounded-md bg-[#07074D] py-[10px] px-8
+                 text-center text-base font-semibold text-white outline-none ml-[530px] mt-5"
                 type="submit"
+                // onClick={() => setInputsOpen(false)}
               />
             </div>
           </form>
